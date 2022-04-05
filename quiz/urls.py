8d53d82817bdfb3f7d5d808cf8d748bd072b1aae
asad <ali48293@gmail.com>
2022-04-06 01:47:46 +0500

    template rendering+logic builidng

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,14 +1,18 @@
 
 from django.contrib import admin
 from django.urls import path , include
-from .views import IndexView,QuizView,CategoryDetailView,PerformanceView
-
-
+from .views import AboutUsView, ContactView, DownloadView, HomeView, IndexView, ProfileView,QuizView,CategoryDetailView,PerformanceView, ResultsView
 
 urlpatterns = [
 
- 
-    path('' , IndexView.as_view() , name='index_view'),
+    path('' , HomeView.as_view() , name='index_view'),
+    path('about/' , AboutUsView.as_view() , name='about'),
+    path('download/' , DownloadView.as_view() , name='download'),
+    path('result/' , ResultsView.as_view() , name='result'),
+    path('contact/' , ContactView.as_view() , name='contact'),
+    path('profile/' , ProfileView.as_view() , name='profile'),
+    
+    path('quiz/' , IndexView.as_view() , name='quiz'),
     path('category/detail/<uuid:id>/' , CategoryDetailView.as_view() , name='category_detail'),
     path('quiz/<uuid:id>/' , QuizView.as_view() , name='quiz_view'),
     path('performance/<uuid:id>/' , PerformanceView.as_view() , name='performace'),
--- quiz/views.py
+++ quiz/views.py
@@ -6,21 +6,40 @@ import json
 from django.http import JsonResponse
 import random
 from django.shortcuts import get_object_or_404
-# Create your views here.
-class IndexView(View):
-    
-    def get(self , request , *args , **kwargs):
 
+class HomeView(View):
+    def get(self , request , *args , **kwargs):
+    
+        return render( request,'home.html')
+class AboutUsView(View):
+    def get(self , request , *args , **kwargs):
+    
+        return render( request,'about.html')
+class DownloadView(View):
+    def get(self , request , *args , **kwargs):
+    
+        return render( request,'download.html')
+class ResultsView(View):
+    def get(self , request , *args , **kwargs):
+        
+        return render( request,'result.html')
+class ContactView(View):
+    def get(self , request , *args , **kwargs):
+        
+        return render( request,'contact_us.html')
+class ProfileView(View):
+    def get(self , request , *args , **kwargs):
+        
+        return render( request,'profile.html')
+class IndexView(View):
+    def get(self , request , *args , **kwargs):
         categories = Category.objects.all() 
         context={
                "categories":categories
         }
-        return render( request,'test_templates/index.html',context=context)
-
+        return render( request,'quiz_new.html',context=context)
 class CategoryDetailView(View):
-
     def get(self , request , *args , **kwargs):
-
         # Initialising the sessions
         request.session['passed_qs']=[]
         request.session['total']=0
@@ -32,22 +51,17 @@ class CategoryDetailView(View):
         context={
                "category":category,
                "questions":questions
-
         }
         return render( request,'test_templates/category_detail.html',context=context)
 
 class QuizView(View):
-
     def get(self , request , *args , **kwargs):
-
         id    = self.kwargs.get("id")
     # Excluding the questions stored in sessions (questions asked before)
         items = list(Question.objects.filter( category = id).exclude(pk__in = request.session['passed_qs']))
-        print("items:",items)
-        if  items:
+        if items:
             print("SESSIONS:",request.session['total'])
             random_item = random.choice(items)
-
             context={
                 "questions":random_item
             }
@@ -65,23 +79,17 @@ class QuizView(View):
             request.session['total']=0
             request.session['score']=0
             return redirect("performace",id=id)
-
 #   Post request for Ajax-call
-    def post(self , request , *args , **kwargs):
-        
+    def post(self , request , *args , **kwargs): 
         data    = json.loads(request.body)
         answer  = data.get("option")
         qid     = data.get("q_id")
-
-        question        = get_object_or_404(Question,pk=qid)
-      
-        
+        question   = get_object_or_404(Question,pk=qid)
         correct = question.correct_answer.option_txt == answer
         if correct:  
             request.session['score']= request.session['score']+1
             request.session['total']= request.session['total']+1
             request.session['passed_qs'].append(qid)
- 
             data={
                 'class':'alert-success',
                 'msg':'Correct Answer, Keep it up '
@@ -91,20 +99,16 @@ class QuizView(View):
         request.session['passed_qs'].append(qid)
         request.session['total']= request.session['total']+1
         data={
-                'class':'alert-danger',
-                'msg':f'Wrong Answer, the correct answer is "{question.correct_answer.option_txt}"'
+            'class':'alert-danger',
+            'msg':f'Wrong Answer, the correct answer is "{question.correct_answer.option_txt}"'
             }
-
         return JsonResponse(safe= False , data =data)  
-
 class PerformanceView(View):
 
     def get(self,request,*args,**kwargs):
-
         id=self.kwargs.get("id")
         total_questions = Question.objects.filter( category__id= id)
-        score = ScoreCard.objects.filter(category=id,User=request.user)
-       
+        score = ScoreCard.objects.filter(category=id,User=request.user) 
         context={
             "score_card":score,
             "total_questions":total_questions,
